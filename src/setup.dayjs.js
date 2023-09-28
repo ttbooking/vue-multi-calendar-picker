efc8779bd1405b53ccd98364fef57f2edd1f8d0e
Ромашов Андрей <a.romashov@ttbooking.ru>
2023-09-28 16:59:55 +0200

    fx2

--- src/setup.dayjs.js
+++ src/setup.dayjs.js
@@ -1,6 +1,6 @@
-import dayjs from 'dayjs';
-import isoWeek from 'dayjs/plugin/isoWeek';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
+import dayjs from 'dayjs/esm';
+import isoWeek from 'dayjs/esm/plugin/isoWeek';
+import customParseFormat from 'dayjs/esm/plugin/customParseFormat';
 dayjs.extend(isoWeek);
 dayjs.extend(customParseFormat);
 export default dayjs;
